3db85a0cc4650985faf51b07b7e7a755f65351dc
alinurd <ali.mnrdn14005@gamail.com>
2025-09-28 01:40:47 +0700

    feat(FE) ladding page ok

--- resources/views/frontend/layout/hero.blade.php
+++ resources/views/frontend/layout/hero.blade.php
@@ -522,3 +522,55 @@ document.addEventListener("DOMContentLoaded", () => {
     </div>
   </div>
 </section>
+
+
+
+
+
+<footer>
+    <div class="container footer-content">
+        <!-- Kolom 1 -->
+        <div class="footer-left">
+            <img src="{{ asset('assets/img/logo.png') }}" alt="logo" class="footer-logo">
+            <p>Nikmati suasana sejuk pegunungan dan pemandangan indah khas Puncak Bogor di Villa Aurora, vila eksklusif dengan kapasitas besar (60-100 orang). Cocok untuk acara keluarga, gathering kantor, retret, reuni, maupun kegiatan komunitas.</p>
+        </div>
+
+        <!-- Kolom 2 -->
+        <div class="footer-links">
+            <h4>Link Terkait</h4>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Terms of service</a></li>
+                <li><a href="#">Privacy policy</a></li>
+            </ul>
+        </div>
+
+        <!-- Kolom 3 -->
+        <div class="footer-villa">
+            <h4>The Gallery Villa</h4>
+            <ul>
+                <li><a href="#">Villa Aurora</a></li>
+                <li><a href="#">Villa Esperanza</a></li>
+            </ul>
+        </div>
+
+        <!-- Kolom 4 -->
+        <div class="footer-social">
+            <h4>Our Social Network</h4>
+            <ul>
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="#"><i class="fab fa-skype"></i></a></li>
+                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; Copyright, {{ date('Y') }} <strong>The Gallery Villa</strong>. All Rights Reserved</p>
+    </div>
+</footer>
+
